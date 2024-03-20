4e89faee53c70377ece0e08404b187d17194f0b2
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-03-20 16:55:04 +0000

    Update test_profile.py to use 1200 rows instead of 1000 rows in test_profile_1000()

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -14,7 +14,7 @@ def test_profile_1000(historical_50k_df):
     settings = {
         "link_type": "dedupe_only",
     }
-    linker = DuckDBLinker(historical_50k_df.head(1000), settings)
+    linker = DuckDBLinker(historical_50k_df.head(1200), settings)
 
     linker.profile_columns(
         ["first_name", "postcode_fake", "substr(dob, 1,4)"], top_n=10, bottom_n=5
